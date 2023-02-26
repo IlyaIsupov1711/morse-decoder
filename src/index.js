578203c0a908281721afff41a4d1afd349bd7f31
IlyaIsupov1711 <107679539+IlyaIsupov1711@users.noreply.github.com>
2023-02-26 09:31:58 +0300

    feat: add new index.js file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const space = "**********";
+  const dot = "10";
+  const dash = "11";
+  const arrWordCode = expr.split(space);
+  const arrWordDecode = arrWordCode.map((item) => 
+  {
+    let arrDecode = [];
+    for (let i = 0; i < item.length; i += 10) {
+      const decode = item
+        .slice(i, i + 10)
+        .replace(/10/g, ".")
+        .replace(/11/g, "-")
+        .replace(/00/g, "");
+      arrDecode.push(MORSE_TABLE[decode]);
+    }
+    return arrDecode.join("");
+  });
+  return arrWordDecode.join(" ");
 }
 
 module.exports = {
